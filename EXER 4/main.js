cf7296f1996bf19b836c89197dc60b88b696c73f
Rodrigo-Oak <132356249+Rodrigo-Oak@users.noreply.github.com>
2023-05-31 13:24:31 -0300

    Add files via upload

---
+++ EXER 4/main.js
@@ -0,0 +1,16 @@
+
+
+
+    let base = prompt ("o usuario prefere musica nacional ou internacional?");
+        if(base == "nacional"){
+           window.location.href = "https://www.youtube.com/watch?v=FxVRXGraRtc&pp=ygUXYm9yYm9sZXRhcyB2aWN0b3IgZSBsZW8%3D";
+        }
+
+        else if(base == "internacional") {
+            window.location.href = "https://www.youtube.com/watch?v=rYEDA3JcQqw&pp=ygUTcm9sbGluZyBpbiB0aGUgZGVlcA%3D%3D";
+        }
+
+        else {
+            alert("Invalido!!");
+        }
+
---
+++ exer 1/main.js
@@ -0,0 +1,30 @@
+
+
+ 
+ 
+
+
+
+            function executanto()
+            {
+            var x;
+            
+            var nome=prompt("Digite seu nome:");
+            
+           
+                if (nome!=null)
+              {
+              x="bem vindo, " + nome + " CPF: " + cpf;              };
+              document.getElementById("demo").innerHTML=x;
+              }
+              var nome=prompt("Digite seu cpf:");
+              {
+          
+                if (cpf!=null)
+              document.getElementById("demo").innerHTML=x;
+              }
+
+            
+
+
+            
